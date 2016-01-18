dbbb9fa22246dfa09d0d178b959d78e17b2c15f0
Daniel Ruschel Dutra <druscheld@gmail.com>
2016-01-18 14:22:04 -0200

    Added a top level script to run the pipeline
    - The script gireds.py is to be the top level interface between the
    various data manipulation functions and the user, along with the
    central hub for directory structuring and file association. It is
    designed to work with a comprehensive configuration file through
    the ConfigParser module.
    
    - example.cfg is an example of such a configuration file, containing
    directory definitions (at this point), options for the reduction
    process, and basically anything we would want the final user to be able
    to change.

---
+++ gireds.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python
+
+import os
+import ConfigParser
+import time
+
+class pipeline():
+    """
+    GIREDS - Gmos Ifu REDuction Suite
+    
+    This class is intended to concentrate and streamline all the of the
+    reduction steps for the reduction of Integral Fiedl Unit (IFU) data
+    from GMOS.
+    """
+
+    def __init__(self, config_file):
+
+        config = ConfigParser.SafeConfigParser()
+        config.read(config_file)
+        self.cfg = config
+
+        # Define directory structure
+        self.root_dir = config.get('main', 'root_dir')
+        self.raw_dir = config.get('main', 'raw_dir')
+        self.products_dir = config.get('main', 'products_dir')
+        self.run_dir = self.products_dir + time.strftime('%Y-%M-%dT%H:%M:%S')
+
+        try:
+            os.mkdir(self.products_dir)
+        except OSError as err:
+            if err.errno == 17:
+                pass
+            else:
+                raise e
+
+        os.mkdir(self.run_dir)
+
+
+    def associate_files(self):
+       
+        
+
+
+
+if __name__ == "__main__":
+    import sys
+    pip = pipeline(sys.argv[1])
+    #pip.associate_files()
