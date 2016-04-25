0f107a39286dd82f51daa49868fa8995261c1d0f
Daniel Ruschel Dutra <druscheld@gmail.com>
2016-04-25 18:02:32 -0300

    Added bin directory and gireds executable script

--- setup.py
+++ setup.py
@@ -7,11 +7,12 @@ packdata = {}
 packdata['gireds'] = ['example.cfg', '.version', 'data/*']
 
 setup(name='gireds',
-      version='0.1.0',
+      version=__version__,
       packages=['gireds',
                 'gireds.utils',
                 'gireds.scripts',
                 'gireds.pipeline'],
+      scripts=['bin/gireds'],
       author='Daniel Ruschel Dutra',
       author_email='druscheld@gmail.com',
       url='https://git.cta.if.ufrgs.br/ruschel/gireds',
