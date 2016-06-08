4c3b40525deaa63eed4eea4b5202af33eed27e3c
Daniel Ruschel Dutra <druscheld@gmail.com>
2016-06-08 10:31:19 -0300

    stdstar class in fluxcal and sensfunc surrogate
    - A class for storing the information read from the output of IRAF's
      standard class has been created. This will facilitate the
      implementation of multiple star fitting for the sensibility function.
    - The sensfunc function has been implemented as a substitute for IRAF's
      sensfunc task. The purpose of this function is to fit sensibility
      function which much finer detail than its IRAF counter part. The
      latter seems to have problems when fitting higher order polynomials,
      leaving out any high frequency oscillations.

--- gireds/utils/fluxcal.py
+++ gireds/utils/fluxcal.py
@@ -1,8 +1,53 @@
 import numpy as np
 import pyfits as pf
-import spectools as st
+import ifscube.spectools as st
 from scipy.optimize import minimize
 from scipy.interpolate import interp1d
+
+
+class stdstar():
+    """
+    A class for the data output of IRAF's standard task.
+    """
+
+    def __init__(self, std):
+
+        self.title = std[0].split()
+        self.exptime, self.airmass = [float(self.title[i]) for i in [3, 4]]
+
+        self.wl, self.flambda, self.bandpass, self.adu =\
+            np.loadtxt(std, skiprows=1, unpack=True)
+
+        self.c = 2.5 * np.log10(
+            self.adu / self.exptime / self.bandpass / self.flambda)
+
+        return
+
+    def extinction(self, extfile):
+        """
+        Not working yet!
+        """
+
+        # e = np.loadtxt(extfile)
+        # ext = interp1d(e[:, 0], e[:, 1])
+        # c = 2.5 * (np.log10(obscounts / exptime / bp / flambda)) + \
+        #            airmass * ext(wl)
+
+        pass
+
+    def interp(self, bounds_error=False, fill_value=np.nan):
+
+        self.adu_interp = interp1d(
+            self.wl, self.adu, bounds_error=bounds_error,
+            fill_value=fill_value)
+        self.flambda_interp = interp1d(
+            self.wl, self.flambda, bounds_error=bounds_error,
+            fill_value=fill_value)
+        self.c_interp = interp1d(
+            self.wl, self.c, bounds_error=bounds_error,
+            fill_value=fill_value)
+
+        return
 
 
 def sensitivity(observed, reference, extinction=None, fnuzero=3.68e-20):
@@ -26,7 +71,7 @@ def sensitivity(observed, reference, extinction=None, fnuzero=3.68e-20):
         sum(obsdata[(obswl > refwl[i] - bp[i]/2.) & (obswl < refwl[i] +
                     bp[i]/2.)]) for i in range(len(refwl))])
 
-    flambda = fnuzero * 10 ** (-0.4 * mag) * 2.99792458e+18 / refwl**2
+    flambda = fnuzero * 10 ** (-0.4 * mag) * 2.99792458e+18 / refwl ** 2
 
     if extinction is None:
         c = 2.5 * np.log10(obsbp / exptime / bp / flambda)
@@ -37,3 +82,37 @@ def sensitivity(observed, reference, extinction=None, fnuzero=3.68e-20):
             airmass * ext(refwl)
 
     return refwl, obsbp, c, flambda
+
+
+def sensfunc(std, extinction=None, fnuzero=3.68e-20, mask=None,
+             shift=False):
+    """
+    Fits a sensibility function to a table provided by IRAF's
+    standard task.
+    """
+
+    with open(std, 'r') as f:
+        fl = f.readlines()
+
+    idx = [i for i in range(len(fl)) if '.fits' in fl[i]]
+    stars = []
+    
+    for i, j in enumerate(idx):
+        if j != idx[-1]:
+            stars.append(stdstar(fl[j:idx[i + 1]]))
+        else:
+            stars.append(stdstar(fl[j:]))
+
+    if len(stars) > 1:
+        for i in stars:
+            i.interp()
+
+        x = np.unique(np.concatenate([i.wl for i in stars]))
+        y = np.nanmean([s.adu_interp(x) for s in stars], 0)
+
+    else:
+        x = stars[0].wl
+        y = stars[0].adu
+
+
+    return x, y
