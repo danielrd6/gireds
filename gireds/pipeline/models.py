18012c9abb48c0e5480fa7960a9686c21d54a2c6
danielrd6 <daniel@astro.ufsc.br>
2021-04-10 19:49:50 -0300

    Updated refraction function
    After much consideration I finally decided it was time to have a
    physically motivated function for the differential refraction of the
    atmosphere.
    
    I found a nice paper on the subject by A. Filippenko (1982, PASP 94,
    715) which presents a very practical formula as well as a table with
    typical values.
    
    Filippenko's equation was implemented as a FittableModel, which is
    then used to fit the offsets across the dispersion direction. The only
    caveat is that it is necessary to include another parameter, to account
    for the field rotation in relation to the parallactic angle.
    
    Also, it is important to remember to convert the offsets from pixels
    to arc seconds, or else the parameter limits won't make any sense.

---
+++ gireds/pipeline/models.py
@@ -0,0 +1,46 @@
+import numpy as np
+from astropy.modeling import Fittable1DModel, Parameter
+from scipy.spatial.transform import Rotation
+
+
+class DifferentialRefraction(Fittable1DModel):
+    """
+    See Filippenko, A. 1982 (PASP 94, 715).
+
+    Wavelengths must be in microns!
+    """
+    n_inputs = 1
+    n_outputs = 1
+
+    temperature = Parameter(default=7.0, min=-60.0, max=40.0)
+    pressure = Parameter(default=500.0, min=100.0, max=800.0)
+    water_vapour = Parameter(default=8.0, min=0.0, max=800.0)
+    air_mass = Parameter(default=1.5, min=1.0, max=5.0)
+    cos_angle = Parameter(default=0.0, min=-1.0, max=1.0)
+    wl_0 = Parameter(default=5000.0)
+
+    @staticmethod
+    def evaluate(x, temperature, pressure, water_vapour, air_mass, cos_angle, wl_0):
+
+        def n_lambda(wavelength):
+            r = np.square(1.0 / wavelength)
+            a = 29498.1 / (146.0 - r)
+            b = 255.4 / (41.0 - r)
+            n_sea_level = 1e-6 * (64.328 + a + b)
+
+            a = 1.0 + (1.049 - (0.0157 * temperature)) * 1e-6 * pressure
+            b = 720.883 * (1.0 + 0.003661 * temperature)
+            n_tp = n_sea_level * pressure * (a / b)
+
+            a = 0.0624 - (0.000680 * r)
+            b = 1.0 + (0.003661 * temperature)
+            water_factor = water_vapour * (a / b) * 1e-6
+
+            return n_tp - water_factor
+
+        k = np.tan(np.arcsin(1 / air_mass))
+        delta_r = 206265.0 * (n_lambda(x * 1e-4) - n_lambda(wl_0 * 1e-4)) * k * cos_angle
+        # z = np.zeros_like(delta_r)
+        # rot = Rotation.from_euler('z', angle)
+        # v = rot.apply(np.column_stack([delta_r, z, z]))[:, 0]
+        return delta_r
